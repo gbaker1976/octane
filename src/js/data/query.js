02bcf0e9fa1cdfeb2982cd61d74a3d54c68c0b73
Garrett Baker <garrett.baker@gmail.com>
2016-04-05 22:54:29 -0400

    added conduit data change logic

--- src/js/data/query.js
+++ src/js/data/query.js
@@ -1,8 +1,75 @@
 'use strict';
 
 class Conduit {
-	constructor(){
+	constructor(data){
+    	this.data = data;
+    };
 
+    setData(data){
+    	let self = this;
+    	Object.keys(this.data).forEach((k) => {
+        	if ( data[k] ) {
+                if ( data[k] !== self.data[k] ) {0
+					self.handlers.forEach((expr, h) => {
+                    	if ( expr[0](data) ) {
+							h(data);
+                        }
+                    });
+                }
+                self.data[k] = data[k];
+            }
+        });
+    };
+
+    registerQueryHandler(handler, ...queries){
+    	let self = this;
+    	if ( !handler || !queries || !queries.length ) {
+      	return;
+      }
+
+      if ( !this.handlers ) {
+      	this.handlers = new Map();
+      }
+
+      if (!this.handlers.has(handler)) {
+        this.handlers.set( handler, [] );
+      }
+
+      queries.forEach((q) => {
+		self.handlers.get(handler).push( self.compileExpressionsForHandler( q.ast.expressions, handler ) );
+      });
+    };
+
+    compileExpressionsForHandler(exprs, handler){
+        let buf = '';
+
+    	exprs.forEach((expr) => {
+            let op = '';
+            let join = '';
+            let joinMap = {
+                'and': '&&',
+              'or': '||'
+            };
+            let opMap = {
+                'equality': '==='
+            };
+
+            if ( expr.joinType ) {
+                join = joinMap[ expr.joinType ];
+            } else {
+                join = '';
+            }
+
+            buf += ' ' + join;
+
+            if ( expr.operand ) {
+                op = opMap[expr.operand];
+            }
+
+            buf += [' d.', expr.leftOp, op, '"', expr.rightOp, '"'].join('');
+        });
+
+        return eval( '(function(d){var r=false;r=(' + buf + ');return r;});' );
     };
 };
 
@@ -163,4 +230,7 @@ class Query {
 
 //var q = new Query('mand="foo" and porchid="rand" or flower="orchid"');
 var q = Query.merge( 'or', new Query('name="fred"'), new Query('dog="rover"') );
-console.log(q.ast);
+var handler = (d) => { console.log(d.name) };
+var c = new Conduit({name: 'barney'})
+c.registerQueryHandler(handler, q);
+c.setData({name: 'fred'});
