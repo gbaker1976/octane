ff906c832ea43be0d64470b9a7396beda932a9d4
Garrett Baker <garrett.baker@gmail.com>
2017-02-08 23:16:11 -0500

    finished up complex selectors, multiple attributes, and embedded commas in attribute values

--- test/css-ast.js
+++ test/css-ast.js
@@ -27,5 +27,152 @@ describe( 'CSS AST Parser', () => {
 		done();
       });
 
+	  it( 'should parse multiple selector css into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					selectors: [
+						'#foo',
+						'#bar'
+					],
+					attributes: [
+						{
+							name: 'margin',
+							value: '0 0 0 0'
+						}
+					]
+				}
+			]
+		};
+		let css = "#foo, #bar {margin: 0 0 0 0}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse multiple complex selectors css into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					selectors: [
+						'#foo > bar + [dog]',
+						"fred[cat~='feline'] > :active"
+					],
+					attributes: [
+						{
+							name: 'margin',
+							value: '0 0 0 0'
+						}
+					]
+				}
+			]
+		};
+		let css = "#foo > bar + [dog], fred[cat~='feline'] > :active {margin: 0 0 0 0}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse multiple complex selectors css into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					selectors: [
+						'#foo > bar + [dog]',
+						"fred[cat~='feline'] > :active"
+					],
+					attributes: [
+						{
+							name: 'margin',
+							value: '0 0 0 0'
+						}
+					]
+				},
+				{
+					selectors: [
+						'body',
+						"a > :hover"
+					],
+					attributes: [
+						{
+							name: 'border',
+							value: 'solid 1px #000'
+						}
+					]
+				}
+			]
+		};
+		let css = "#foo > bar + [dog], fred[cat~='feline'] > :active {margin: 0 0 0 0}\n\n \tbody, a > :hover {border: solid 1px #000}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse multiple complex selectors and multiple attributes into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					selectors: [
+						'#foo > bar + [dog]',
+						"fred[cat~='feline'] > :active"
+					],
+					attributes: [
+						{
+							name: 'margin',
+							value: '0 0 0 0'
+						},
+						{
+							name: 'padding',
+							value: '10px 100px 3em 4rem'
+						}
+					]
+				},
+				{
+					selectors: [
+						'body',
+						"a > :hover"
+					],
+					attributes: [
+						{
+							name: 'border',
+							value: 'solid 1px #000'
+						}
+					]
+				}
+			]
+		};
+		let css = "#foo > bar + [dog], fred[cat~='feline'] > :active {margin: 0 0 0 0;\npadding: 10px 100px 3em 4rem;}\n\n \tbody, a > :hover {border: solid 1px #000}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse embedded commas in attribute value into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					selectors: [
+						'#foo'
+					],
+					attributes: [
+						{
+							name: 'background',
+							value: 'rgb(0, 0, 0, 0)'
+						}
+					]
+				}
+			]
+		};
+		let css = "#foo {background: rgb(0, 0, 0, 0)}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
   	});
 });
