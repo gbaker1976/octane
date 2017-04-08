cb763a2867ff326ab2e7d57ce5e099b6b49c2d46
Garrett Baker <garrett.baker@gmail.com>
2017-04-08 15:47:32 -0400

    fixed comment parsing

--- test/css-ast.js
+++ test/css-ast.js
@@ -8,15 +8,18 @@ describe( 'CSS AST Parser', () => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo'
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'margin',
 							value: '0 0 0 0'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
@@ -31,16 +34,19 @@ describe( 'CSS AST Parser', () => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo',
 						'#bar'
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'margin',
 							value: '0 0 0 0'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
@@ -55,16 +61,19 @@ describe( 'CSS AST Parser', () => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo > bar + [dog]',
 						"fred[cat~='feline'] > :active"
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'margin',
 							value: '0 0 0 0'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
@@ -75,32 +84,38 @@ describe( 'CSS AST Parser', () => {
 		done();
       });
 
-	  it( 'should parse multiple complex selectors css into AST', ( done ) => {
+	  it( 'should parse multiple declaration with multiple complex selectors css into AST', ( done ) => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo > bar + [dog]',
 						"fred[cat~='feline'] > :active"
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'margin',
 							value: '0 0 0 0'
 						}
-					]
+					],
+					value: ''
 				},
 				{
+					type: 1,
 					selectors: [
 						'body',
 						"a > :hover"
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'border',
 							value: 'solid 1px #000'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
@@ -115,32 +130,39 @@ describe( 'CSS AST Parser', () => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo > bar + [dog]',
 						"fred[cat~='feline'] > :active"
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'margin',
 							value: '0 0 0 0'
 						},
 						{
+							type: 4,
 							name: 'padding',
 							value: '10px 100px 3em 4rem'
 						}
-					]
+					],
+					value: ''
 				},
 				{
+					type: 1,
 					selectors: [
 						'body',
 						"a > :hover"
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'border',
 							value: 'solid 1px #000'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
@@ -155,19 +177,124 @@ describe( 'CSS AST Parser', () => {
 		let expected = {
 			doc: [
 				{
+					type: 1,
 					selectors: [
 						'#foo'
 					],
 					attributes: [
 						{
+							type: 4,
 							name: 'background',
 							value: 'rgb(0, 0, 0, 0)'
 						}
-					]
+					],
+					value: ''
 				}
 			]
 		};
 		let css = "#foo {background: rgb(0, 0, 0, 0)}";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse embedded comments into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					type: 2,
+					selectors: [],
+					attributes: [],
+					value: 'this is a comment'
+				},
+				{
+					type: 1,
+					selectors: [
+						'#foo'
+					],
+					attributes: [
+						{
+							type: 4,
+							name: 'background',
+							value: 'rgb(0, 0, 0, 0)'
+						}
+					],
+					value: ''
+				}
+			]
+		};
+		let css = "/* this is a comment */ #foo { background: rgb(0, 0, 0, 0) }";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should parse multiple embedded comments into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					type: 2,
+					selectors: [],
+					attributes: [],
+					value: 'this is a comment'
+				},
+				{
+					type: 2,
+					selectors: [],
+					attributes: [],
+					value: 'this is another comment'
+				},
+				{
+					type: 1,
+					selectors: [
+						'#foo'
+					],
+					attributes: [
+						{
+							type: 4,
+							name: 'background',
+							value: 'rgb(0, 0, 0, 0)'
+						}
+					],
+					value: ''
+				}
+			]
+		};
+		let css = "/* this is a comment *//* this is another comment */ #foo { background: rgb(0, 0, 0, 0) }";
+		let actual = cssAst( css );
+
+		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
+		done();
+      });
+
+	  it( 'should not parse embedded comments in declarations into AST', ( done ) => {
+		let expected = {
+			doc: [
+				{
+					type: 2,
+					selectors: [],
+					attributes: [],
+					value: 'this is a comment'
+				},
+				{
+					type: 1,
+					selectors: [
+						'#foo'
+					],
+					attributes: [
+						{
+							type: 4,
+							name: 'background',
+							value: 'rgb(0, 0, 0, 0)'
+						}
+					],
+					value: ''
+				}
+			]
+		};
+		let css = "/* this is a comment */ #foo { /* this is another comment */background: rgb(0, 0, 0, 0);/* this is another comment */ }";
 		let actual = cssAst( css );
 
 		assert.deepEqual( actual, expected, 'Result of parse does not match!' );
